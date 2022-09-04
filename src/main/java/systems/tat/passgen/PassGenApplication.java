7c413e680e9f0da63e0764fc852e8944f86f6b2f
Niklas Tat <2548340-N.Tat@users.noreply.gitlab.com>
2022-09-04 20:08:18 +0200

    - Adding more files to gitignore - Add Spring Boot and default files - Add Gradle to project

---
+++ src/main/java/systems/tat/passgen/PassGenApplication.java
@@ -0,0 +1,12 @@
+package systems.tat.passgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassGenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassGenApplication.class, args);
+    }
+}
